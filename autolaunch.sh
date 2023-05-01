d1791792613281f470b1275b5b22cfef2ad57972
Gian Sanchez <giansanchez71@gmail.com>
2023-05-02 04:37:19 +0800

    Script change

--- autolaunch.sh
+++ autolaunch.sh
@@ -2,12 +2,13 @@
 
 # Start the Python server
 lxterminal -t "Python Server" -e "bash -c 'python3 ~/untitled-pyscript/script.py; $SHELL'"
+lxterminal -t "Web Server" -e "bash -c 'serve ~/untitled-pyscript/client/build; $SHELL'"
 
 # Wait for the server to start up
 sleep 2
 
 # Open the Raspberry Pi OS default browser to localhost:5000
-xdg-open "http://localhost:5000"
+xdg-open "http://localhost:3000"
 
 # Keep the script running to keep the terminal window open
 while true; do
