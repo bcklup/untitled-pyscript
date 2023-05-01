4514b9a0883cdbb56b050502528a8f9864cfd8d5
Gian Sanchez <giansanchez71@gmail.com>
2023-05-01 17:39:17 +0800

    Auto-launch browser in script

--- autolaunch.sh
+++ autolaunch.sh
@@ -6,6 +6,9 @@ lxterminal -t "Python Server" -e "bash -c 'python3 script.py; $SHELL'"
 # Wait for the server to start up
 sleep 2
 
+# Open the Raspberry Pi OS default browser to localhost:5000
+xdg-open "http://localhost:5000"
+
 # Keep the script running to keep the terminal window open
 while true; do
     sleep 1
